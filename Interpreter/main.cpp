ca9ab549a2e6408157bbc34b027dd612d98d2492
June-UwU <williamreji66@gmail.com>
2023-04-22 00:13:54 +0530

    first commit

---
+++ Interpreter/main.cpp
@@ -0,0 +1,8 @@
+#include "types.hpp"
+#include <iostream>
+
+s32 main(s32 argc, char** argv) 
+{
+    std::cout << "hello world";
+    return 0;
+}
---
+++ Interpreter/types.hpp
@@ -0,0 +1,17 @@
+#include <cstdint>
+
+// SIGNED TYPES
+typedef int8_t      s8;
+typedef int16_t     s16;
+typedef int32_t     s32;
+typedef int64_t     s64;
+
+// UNSIGNED TYPES
+typedef uint8_t     u8;
+typedef uint16_t    u16;
+typedef uint32_t    u32;
+typedef uint64_t    u64;
+
+// FLOATING TYPES
+typedef float       f32;
+typedef double      f64;
